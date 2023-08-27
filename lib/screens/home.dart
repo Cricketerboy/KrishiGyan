07154b2c579fc4c2a1d4ce85a6e3dcec041eb901
Mohit Anand <praveenanand801@gmail.com>
2023-08-27 09:09:02 +0530

    initial commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -65,157 +65,157 @@ class _HomeState extends State<Home> {
           currentIndex: _selectedIndex,
           onTap: _onItemTapped,
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  MapScreen(),
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: SearchScreen(),
+        body:  Column(
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    MapScreen(),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        padding: EdgeInsets.all(10.0),
+                        child: SearchScreen(),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(23.0),
-                topRight: Radius.circular(23.0),
-              ),
-              child: Container(
-                height: 390,
-                width: 360,
-                color: Colors.white,
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 10),
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: 70,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 10,
-                            itemBuilder: ((context, index) => Category()),
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(23.0),
+                  topRight: Radius.circular(23.0),
+                ),
+                child: Container(
+                  height: 390,
+                  width: 360,
+                  color: Colors.white,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        Container(
+                          margin:
+                              EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: 70,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 10,
+                              itemBuilder: ((context, index) => Category()),
+                            ),
                           ),
                         ),
-                      ),
-                      Divider(),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 22, vertical: 8),
-                        child: Text(
-                          'Available Drone Spraying',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Divider(),
-                      Container(
-                        margin: EdgeInsets.all(5.0),
-                        width: 350,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 238, 234, 234),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: ListTile(
-                          leading: Image.asset("assets/images/drone.jpg",
-                              width: 30, height: 30),
-                          title: Text(
-                            'Krishakti drones with smart',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          subtitle: Text(
-                            'nozzle spraying starting from 56',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                          trailing: Icon(Icons.arrow_forward),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(5.0),
-                        width: 350,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 238, 234, 234),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: ListTile(
-                          leading: Image.asset("assets/images/drone.jpg",
-                              width: 30, height: 30),
-                          title: Text(
-                            'Krishakti drones with smart',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          subtitle: Text(
-                            'nozzle spraying starting from 56',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                          trailing: Icon(Icons.arrow_forward),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(5.0),
-                        width: 350,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 238, 234, 234),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: ListTile(
-                          leading: Image.asset("assets/images/drone.jpg",
-                              width: 30, height: 30),
-                          title: Text(
-                            'Krishakti drones with smart',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          subtitle: Text(
-                            'nozzle spraying starting from 56',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                          trailing: Icon(Icons.arrow_forward),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 22, vertical: 8),
-                        child: Text(
-                          'Recommended for you',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: 70,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 5,
-                            itemBuilder: ((context, index) =>
-                                Recommendations()),
+                        Divider(),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 22, vertical: 8),
+                          child: Text(
+                            'Available Drone Spraying',
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ),
-                    ],
+                        Divider(),
+                        Container(
+                          margin: EdgeInsets.all(5.0),
+                          width: 350,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 238, 234, 234),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            leading: Image.asset("assets/images/drone.jpg",
+                                width: 30, height: 30),
+                            title: Text(
+                              'Krishakti drones with smart',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            subtitle: Text(
+                              'nozzle spraying starting from 56',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            trailing: Icon(Icons.arrow_forward),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(5.0),
+                          width: 350,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 238, 234, 234),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            leading: Image.asset("assets/images/drone.jpg",
+                                width: 30, height: 30),
+                            title: Text(
+                              'Krishakti drones with smart',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            subtitle: Text(
+                              'nozzle spraying starting from 56',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            trailing: Icon(Icons.arrow_forward),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(5.0),
+                          width: 350,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 238, 234, 234),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            leading: Image.asset("assets/images/drone.jpg",
+                                width: 30, height: 30),
+                            title: Text(
+                              'Krishakti drones with smart',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            subtitle: Text(
+                              'nozzle spraying starting from 56',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            trailing: Icon(Icons.arrow_forward),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 22, vertical: 8),
+                          child: Text(
+                            'Recommended for you',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          margin:
+                              EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: 70,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 5,
+                              itemBuilder: ((context, index) =>
+                                  Recommendations()),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
     );
   }
 }
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -10,30 +10,32 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Color.fromARGB(33, 49, 49, 49)),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search,Area,Locality',
-                errorBorder: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                focusedErrorBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                border: InputBorder.none,
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Color.fromARGB(33, 49, 49, 49)),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search,Area,Locality',
+                  errorBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  focusedErrorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  border: InputBorder.none,
+                ),
               ),
             ),
-          ),
-          InkWell(onTap: () {}, child: Icon(Icons.search)),
-        ],
+            InkWell(onTap: () {}, child: Icon(Icons.search)),
+          ],
+        ),
       ),
     );
   }
